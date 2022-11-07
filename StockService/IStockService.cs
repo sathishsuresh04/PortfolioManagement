704b1e63fb585b69e73a460809806d987d83bfd5
Aleksander Szczepan <aleksander.szczepan@bannerflow.com>
2022-11-07 10:09:48 +0100

    align base/native currency wording

--- StockService/IStockService.cs
+++ StockService/IStockService.cs
@@ -6,18 +6,18 @@ namespace StockService
 {
     public interface IStockService
     {
-        Task<(decimal Price, string NativeCurrency)> GetCurrentStockPrice(string ticker);
+        Task<(decimal Price, string BaseCurrency)> GetCurrentStockPrice(string ticker);
     }
 
     public class StockService : IStockService
     {
-        private readonly List<string> _nativeCurrencies = new() { "USD", "SEK", "NOK", "CAD", "EUR" };
+        private readonly List<string> _baseCurrencies = new() { "USD", "SEK", "NOK", "CAD", "EUR" };
 
-        public async Task<(decimal Price, string NativeCurrency)> GetCurrentStockPrice(string ticker)
+        public async Task<(decimal Price, string BaseCurrency)> GetCurrentStockPrice(string ticker)
         {
             var random = new Random();
 
-            return await Task.FromResult((random.Next(20, 800), _nativeCurrencies[random.Next(0, _nativeCurrencies.Count)]));
+            return await Task.FromResult((random.Next(20, 800), _baseCurrencies[random.Next(0, _baseCurrencies.Count)]));
         }
     }
 }
